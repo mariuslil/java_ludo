26b29c778fc608a16c6cb1cf26bd1086756b0b9f
BredeFK <bredefk@stud.ntnu.no>
2018-11-13 14:24:36 +0100

    Still in progress

--- src/main/java/no/ntnu/imt3281/ludo/logic/DiceEvent.java
+++ src/main/java/no/ntnu/imt3281/ludo/logic/DiceEvent.java
@@ -1,9 +1,24 @@
 package no.ntnu.imt3281.ludo.logic;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class DiceEvent {
+    private Ludo ludo;
+    private int color;
+    private int diceNr;
+    private List<DiceListener> diceListeners;
 
     // TODO : finish this event class
     public DiceEvent(Ludo ludo, int color, int diceNr){
+        this.ludo = ludo;
+        this.color = color;
+        this.diceNr = diceNr;
+        this.diceListeners = new ArrayList<>();
+    }
 
+    @Override
+    public boolean equals(Object obj) {
+        return super.equals(obj);
     }
 }
--- src/main/java/no/ntnu/imt3281/ludo/logic/DiceListener.java
+++ src/main/java/no/ntnu/imt3281/ludo/logic/DiceListener.java
@@ -1,9 +1,10 @@
 package no.ntnu.imt3281.ludo.logic;
 
 public class DiceListener {
+    private DiceEvent event;
 
     // TODO : finish this listener
-    public void diceThrown(DiceEvent event){
-
+    public void diceThrown(DiceEvent event) {
+        this.event = event;
     }
 }
