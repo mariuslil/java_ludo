e8c5e7e169ef4f61e291f5280e8c2bdcea0d6241
BredeFK <bredefk@stud.ntnu.no>
2018-11-14 14:01:18 +0100

    Fixed equals function

--- src/main/java/no/ntnu/imt3281/ludo/logic/DiceEvent.java
+++ src/main/java/no/ntnu/imt3281/ludo/logic/DiceEvent.java
@@ -6,15 +6,29 @@ public class DiceEvent {
     private int color;
     private int diceNr;
 
-    // TODO : finish this event class
-    public DiceEvent(Ludo ludo, int color, int diceNr){
+    /**
+     * @param ludo Ludo
+     * @param color  int
+     * @param diceNr int
+     */
+    public DiceEvent(Ludo ludo, int color, int diceNr) {
         this.ludo = ludo;
         this.color = color;
         this.diceNr = diceNr;
     }
 
+    /**
+     * https://wiki.sei.cmu.edu/confluence/display/java/MET09-J.+Classes+that+define+an+equals%28%29+method+must+also+define+a+hashCode%28%29+method
+     */
     @Override
     public boolean equals(Object obj) {
-        return super.equals(obj);
+        if (obj == this) {
+            return true;
+        }
+        if (!(obj instanceof DiceEvent)) {
+            return false;
+        }
+        DiceEvent event = (DiceEvent) obj;
+        return (event.ludo == ludo) && (event.color == color) && (event.diceNr == diceNr);
     }
 }
