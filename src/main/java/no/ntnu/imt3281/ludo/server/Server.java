e6b18e1bc7277c4dd15a11b4e5ee7b4c49b6d056
Johan Aanesen <johanaan@stud.ntnu.no>
2018-11-17 20:40:00 +0100

    create Database now works

--- src/main/java/no/ntnu/imt3281/ludo/server/Server.java
+++ src/main/java/no/ntnu/imt3281/ludo/server/Server.java
@@ -1,5 +1,9 @@
 package no.ntnu.imt3281.ludo.server;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 /**
  * 
  * This is the main class for the server. 
@@ -10,8 +14,25 @@ package no.ntnu.imt3281.ludo.server;
  */
 public class Server {
 
+    private static void createDB(){
+
+        try (Connection connect = DriverManager.getConnection("jdbc:derby:LudoDB")) {
+            System.out.println("connected");
+        } catch (SQLException e) {
+            System.out.println("Cannot connect, will create");
+            try (Connection connect = DriverManager.getConnection("jdbc:derby:LudoDB;create=true")) {
+                System.out.println("created");
+            } catch (SQLException e1) {
+                System.out.println("Doesnt work: "+e1.getMessage());
+            }
+        }
+
+
+
+    }
+
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		createDB();
 
 	}
 
